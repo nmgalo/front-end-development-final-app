a2ca653cd769dabd1766b3dff38a3d41da50fba8
Nika <nikolozamgalo@gmail.com>
2021-06-30 22:42:42 +0400

    added hotels (with nudes xDD)

--- js/app.js
+++ js/app.js
@@ -45,7 +45,7 @@ if (persons != null) {
       const ul = document.createElement("div");
       ul.classList.add("persons");
 
-      res.data.map((item, index) => {
+      res.data.map((item, _) => {
         let li = document.createElement("div");
         let image = document.createElement("img");
         let span = document.createElement("span");
@@ -56,5 +56,38 @@ if (persons != null) {
         ul.appendChild(li);
       });
       persons.appendChild(ul);
+    });
+}
+
+const hotels = document.getElementById("hotels");
+if (hotels != null) {
+  fetch(
+    "https://api2.mytravel.ge/api/ka/daily/products?LocID=776458944&LocTitle=%E1%83%AB%E1%83%95%E1%83%94%E1%83%9A%E1%83%98%20%E1%83%91%E1%83%90%E1%83%97%E1%83%A3%E1%83%9B%E1%83%98%E1%83%A1%20%E1%83%A3%E1%83%91%E1%83%90%E1%83%9C%E1%83%98&Page=1"
+  )
+    .then((res) => res.json())
+    .then((res) => {
+      let content = "";
+      res.data.actual.products.map((item, _) => {
+        content += `
+            <div>
+              <a href="#">
+                <div>
+                  <img src="./img/ic_star.svg" alt="" />
+                  <img
+                    src="${item.larges[0]}"
+                    alt="Mock item"
+                  />
+                </div>
+                <div class="info">
+                  <span>2 საძინებელი</span>
+                  <p>${JSON.parse(item.full_product_title)["4"]}</p>
+                  <span>${item.one_night_price} ₾ / Night</span>
+                </div>
+              </a>
+            </div>
+        `;
+      });
+
+      hotels.innerHTML = content;
     });
 }
